b3c3913be9db07a17e64acf65a9e6799dc8910cf
Andrew McCluskey <andrew@ajmccluskey.com>
2017-12-15 09:10:52 +1000

    Move everything out of the Game Struct
    I don't know why I thought it was a good idea...

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -7,108 +7,112 @@ MAGIC_SUM = 15
 MAGIC_SQUARE = [8,1,6,3,5,7,4,9,2]
 SYMBOLS = [:naught, :cross]
 
-Game = Struct.new(:board, :next_symbol, :winner) do
-  def self.start
-    Game.new(Game.new_board, MOVES.sample, nil)
-  end
+Game = Struct.new(:board, :next_symbol, :winner)
 
-  def self.new_board
-    (1..CELLS).inject([]) do |arr|
-      arr + [:empty]
-    end
-  end
+def start(first_player)
+  return nil if !MOVES.include?(first_player)
+  Game.new(new_board, first_player, nil)
+end
 
-  def finished?
-    return true unless winner.nil?
-    return true if board.select { |m| m != :empty }.count == CELLS
-    false
+def new_board
+  (1..CELLS).inject([]) do |arr|
+    arr + [:empty]
   end
+end
 
-  # Position is a natural number between 1 and 9 inclusive
-  # representing the square we'd like to place a piece. 1
-  # is top left, 9 is bottom right.
-  def move(position)
-    index = position - 1
-    target = board[index]
+def finished?(game)
+  return true unless game.winner.nil?
+  return true if board_full?(game.board)
+  false
+end
 
-    if target.nil? || target != :empty
-      self
-    else
-      new_board = board.clone
-      new_board[index] = next_symbol
-      new_symbol = next_symbol == :naught ? :cross : :naught
-      Game.new(new_board, new_symbol)
-    end
+def board_full?(board)
+  board.select { |s| s != :empty }.count == CELLS
+end
+
+# Position is a natural number between 1 and 9 inclusive
+# representing the square we'd like to place a piece. 1
+# is top left, 9 is bottom right.
+def move(game, position)
+  index = position - 1
+  target = game.board[index]
+
+  if target.nil? || target != :empty
+    self
+  else
+    new_board = game.board.clone
+    new_board[index] = game.next_symbol
+    new_symbol = game.next_symbol == :naught ? :cross : :naught
+    Game.new(new_board, new_symbol, winner(new_board))
   end
+end
 
-  def draw
-    bars = '-' * (3 * BOARD_SIZE + (BOARD_SIZE - 1))
-    intercalate(Game.rows(board).map { |r| Game.draw_row(r) }, bars).join("\n")
-  end
+def draw(board)
+  bars = '-' * (3 * BOARD_SIZE + (BOARD_SIZE - 1))
+  intercalate(rows(board).map { |r| draw_row(r) }, bars).join("\n")
+end
 
-  # [Move] -> Int -> Maybe Move
-  def winner
-    winning_pair =
-      board.zip(MAGIC_SQUARE).
-            select { |(s,m)| SYMBOLS.include?(s) }.
-            group_by { |(s,m)| s }.
-            select { |s, ms|
-              ms.map(&:last).permutation(3).any? { |triples| triples.reduce(&:+) == MAGIC_SUM
-            }}.
-            first
-    winning_pair.nil? ? nil : winning_pair.first
-  end
+def winner(board)
+  winning_pair =
+    board.zip(MAGIC_SQUARE).
+          select { |(s,m)| SYMBOLS.include?(s) }.
+          group_by { |(s,m)| s }.
+          select { |s, ms|
+            ms.map(&:last).permutation(3).any? { |triples| triples.reduce(&:+) == MAGIC_SUM
+          }}.
+          first
+  winning_pair.nil? ? nil : winning_pair.first
+end
 
-  def self.draw_row(row)
-    ' ' + row.map { |c| "#{draw_symbol(c)}" }.join(' | ')
-  end
+def draw_row(row)
+  ' ' + row.map { |c| "#{draw_symbol(c)}" }.join(' | ')
+end
 
-  def self.draw_symbol(s)
-    case s
-    when :empty
-      ' '
-    when :cross
-      'x'
-    when :naught
-      'o'
-    else
-      "INVALID SYMBOL #{s.to_s}"
-    end
+def draw_symbol(s)
+  case s
+  when :empty
+    ' '
+  when :cross
+    'x'
+  when :naught
+    'o'
+  else
+    "INVALID SYMBOL #{s.to_s}"
   end
+end
 
-  def self.rows(board)
-    board.each_slice(BOARD_SIZE).to_a
-  end
-
-  def with_winner(w)
-    Game.new(board, next_symbol, w)
-  end
+def rows(board)
+  board.each_slice(BOARD_SIZE).to_a
 end
 
 def intercalate(l, e)
   l.inject([]) { |a, x| a << x << e }[0..-2]
 end
 
+##########################################
+# HERE'S WHERE WE GET REFERENTIALLY OPAQUE
+##########################################
+
 puts `clear`
 puts "Let's play!"
 gets
 puts `clear`
-game = Game.start
-until game.finished?
-  puts game.draw
+game = start(MOVES.sample)
+until finished?(game)
+  puts draw(game.board)
   puts
   puts "It's #{game.next_symbol}'s move."
   puts "move (1-9): "
   move = gets.chomp
   puts `clear`
   if move =~ /[1-9]/
-    game = game.move(move.to_i)
+    game = move(game, move.to_i)
   else
     puts "'#{move}' is not a valid move. Try 1-9."
   end
 end
 
-puts game.draw
+puts draw(game.board)
 puts
 puts "Game over!"
 puts "Congratulations #{game.winner}!!!" if game.winner
